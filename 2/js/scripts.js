87f418d772318008900c2b7af321d92fdcbcce93
Shaggy8 <rominus@bigmir.net>
2016-01-15 23:17:22 +0200

    ай да я!)) Signed-off-by: Shaggy8 <rominus@bigmir.net>

---
+++ 2/js/scripts.js
@@ -0,0 +1,20 @@
+var names = [];
+
+for (var i = 0; i < 5; i++) {
+	names.push( prompt('Введіть ім\'я користувача', '') );
+}
+
+var guest = prompt('Назвіться, будь ласка', '');
+var count = 0;
+
+for (var k = 0; k < names.length; k++) {
+	if (guest == names[k]) {
+		count++
+		alert('Вітаю, ' + guest +
+			'! А ви знаєте, що це накодив Рома? Правда ж він молодець?)))');
+	}
+}
+
+if (!count) {
+	alert('Ану геть звідси, не знаю таких!');
+}
